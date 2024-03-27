7032ad4191232ab3bab9e81a3dc17e8daaed8b16
Jan Kreps <jan@kreps.cz>
2024-03-27 17:18:52 +0100

    Update provision.sh

--- php-ldap/provision.sh
+++ php-ldap/provision.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 echo "Setting up php-ldap and php7.2-ldap"
-apt-get --assume-yes install php-ldap php7.2-ldap
+# apt-get --assume-yes install php-ldap php7.2-ldap
+apt-get --assume-yes install php-ldap
